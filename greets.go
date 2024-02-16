99351797b4576640a14399fa88009773eda34ae4
Phil Shaughnessy <philip.shaughnessy@gmail.com>
2024-02-16 09:58:31 -0500

    Try new go 1.22 http routing

--- greets.go
+++ greets.go
@@ -1,14 +1,18 @@
 package main
 
+var (
+	thing string = "hello"
+)
+
 func hi(s string) string {
-  greet := "Hello"
-  if s != "" {
-    greet += " " + s
-  }
-  return greet + "!"
+	greet := "Hello"
+	if s != "" {
+		greet += " " + s
+	}
+	return greet + "!"
 }
 
 func bye(s string) string {
-  farewell := "Goodbye"
-  return farewell + " " + s
+	farewell := "Goodbye"
+	return farewell + " " + s
 }
--- main.go
+++ main.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	port = 5432
 )
 
 func main() {
 	fmt.Println(hi("World"))
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /try/{cmd}", func(w http.ResponseWriter, r *http.Request) {
+		cmd := r.PathValue("cmd")
+		if cmd == "panic" {
+			panic("beep boop bop")
+		}
+		fmt.Fprintf(w, "got cmd: %v", cmd)
+	})
+
+	fmt.Printf("Listening on: %v", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%v", port), mux); err != nil {
+		log.Fatalf("uh oh!")
+	}
+
 	fmt.Println(bye("for now!"))
 }
